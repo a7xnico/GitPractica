3f1080de8985cd3cbe6416ae94f0e8001328ca1c
Nicolas <pablonicolasarayamaza@gmail.com>
2025-04-28 19:59:07 -0300

    Segundo commit

--- main.c
+++ main.c
@@ -4,5 +4,10 @@
 int main()
 {
     printf("Hello world!\n");
+    printf("Hello world!\n");
+    printf("Hello world!\n");
+    printf("Hello world!\n");
+    printf("Hello world!\n");
+    printf("Hello world!\n");
     return 0;
 }
